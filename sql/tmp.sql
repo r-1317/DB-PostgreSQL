20b0763bad2f0561ad6bd036b37a08b05852d192
r1317 <122949375+r-1317@users.noreply.github.com>
2025-10-24 11:15:34 +0900

    1024-1115

--- sql/tmp.sql
+++ sql/tmp.sql
@@ -1,6 +1,15 @@
+START TRANSACTION;
+
+INSERT INTO
+  s_characters (name, job, level, buff, guild, last_login_at, created_on)
+VALUES
+  ('Zoe', 'Paladin', 11, 0.00, 'W.Wind', '2025-10-20 15:48:00', '2025-10-12'),
+  ('Justin', 'Archer', 2, 0.50, NULL, '2025-10-20 11:28:00', '2025-10-13');
+
 SELECT
   *
 FROM
-  s_characters
-WHERE
-  last_login_at = '2025-10-03 11:42';
+  s_characters;
+
+-- 確認
+ROLLBACK;
