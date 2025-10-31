86caf86e6b21cab432ea31cc629d4c73c1c9860c
r1317 <122949375+r-1317@users.noreply.github.com>
2025-10-31 10:19:17 +0900

    1031-1019

--- sql/05/tmp.sql
+++ sql/05/tmp.sql
@@ -1,5 +1,23 @@
 SELECT
-  COUNT(guild) || '人' AS "ギルド所属",
-  COUNT(*) - COUNT(guild) || '人' AS "無所属"
+  name AS "最も短い名前"
 FROM
-  s_characters;
+  s_characters
+WHERE
+  LENGTH(name) = (
+    SELECT
+      MIN(LENGTH(name))
+    FROM
+      s_characters
+  );
+
+SELECT
+  name AS "最も長い名前"
+FROM
+  s_characters
+WHERE
+  LENGTH(name) = (
+    SELECT
+      MAX(LENGTH(name))
+    FROM
+      s_characters
+  );
