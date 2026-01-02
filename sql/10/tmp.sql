672d680760ffc6f03d56da521d3422e1d59a580d
r1317 <122949375+r-1317@users.noreply.github.com>
2026-01-02 11:30:55 +0900

    0101-1130

--- sql/10/tmp.sql
+++ sql/10/tmp.sql
@@ -1,8 +1,18 @@
 SELECT
   g.guild_id,
-  g.name,
-  c.name AS "owner_name",
-  c.level AS "owner_level"
+  g.name AS "guild",
+  j.name AS "job",
+  COUNT(*)
 FROM
-  x_guilds AS g
-  JOIN x_characters AS c ON g.owner_id = c.character_id;
+  x_characters AS c
+  JOIN x_guild_characters AS gc ON c.character_id = gc.character_id
+  JOIN x_guilds AS g ON g.guild_id = gc.guild_id
+  JOIN x_jobs AS j ON c.job_id = j.job_id
+WHERE
+  c.deleted_at IS NULL
+GROUP BY
+  g.guild_id,
+  j.job_id
+ORDER BY
+  g.guild_id,
+  j.job_id;
