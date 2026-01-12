f3feb523c13859e1f99e97f02382e30c17bc8754
r1317 <122949375+r-1317@users.noreply.github.com>
2026-01-12 13:24:06 +0900

    0112-1324

--- sql/10/tmp.sql
+++ sql/10/tmp.sql
@@ -1,13 +1,49 @@
 SELECT
-  c.character_id,
-  c.name || '( ' || MAX(j.name) || ')' AS "name (job)",
-  SUM(ci.qty) AS total_qty
+  COALESCE(c1.item_id, c2.item_id, c3.item_id) AS "i_id",
+  COALESCE(c1.name, c2.name, c3.name) AS "item_name",
+  c1.qty AS "Marvin",
+  c2.qty AS "Jack",
+  c3.qty AS "Alice",
+  COALESCE(c1.description, c2.description, c3.description) AS "description"
 FROM
-  n_characters AS c
-  RIGHT JOIN n_character_items AS ci ON c.character_id = ci.character_id
-  -- RIGHT JOIN n_items AS i ON ci.item_id = i.item_id
-  LEFT JOIN n_jobs AS j ON c.job_id = j.job_id
-GROUP BY
-  c.character_id
+  (
+    SELECT
+      ci.item_id,
+      i.name,
+      ci.qty,
+      i.description
+    FROM
+      x_character_items AS ci
+      JOIN x_items AS i ON ci.item_id = i.item_id
+      JOIN x_characters AS C ON ci.character_id = c.character_id
+    WHERE
+      c.name = 'Marvin'
+  ) AS c1
+  FULL OUTER JOIN ( -- ◀ 完全外部結合
+    SELECT
+      ci.item_id,
+      i.name,
+      ci.qty,
+      i.description
+    FROM
+      x_character_items AS ci
+      JOIN x_items AS i ON ci.item_id = i.item_id
+      JOIN x_characters AS C ON ci.character_id = c.character_id
+    WHERE
+      c.name = 'Jack'
+  ) AS c2 ON c1.item_id = c2.item_id
+  FULL OUTER JOIN ( -- ◀ 完全外部結合
+    SELECT
+      ci.item_id,
+      i.name,
+      ci.qty,
+      i.description
+    FROM
+      x_character_items AS ci
+      JOIN x_items AS i ON ci.item_id = i.item_id
+      JOIN x_characters AS C ON ci.character_id = c.character_id
+    WHERE
+      c.name = 'Alice'
+  ) AS c3 ON c1.item_id = c3.item_id
 ORDER BY
-  c.character_id
+  COALESCE(c1.item_id, c2.item_id, c3.item_id);
